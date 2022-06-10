46db850fbb072730636e53aa6013c3bf6b55778c
dongnv0808 <yamaraja0808@gmail.com>
2022-06-10 14:50:09 +0700

    done

---
+++ src/test/app.ts
@@ -0,0 +1,8 @@
+export class AbsoluteNumberCalculator{
+    public static findAbsolute(num: number): number{
+        // throw new Error("Unsupported Operation Exception")
+        // return 0;
+        if(num < 0) return -num;
+        return num;
+    }
+}
